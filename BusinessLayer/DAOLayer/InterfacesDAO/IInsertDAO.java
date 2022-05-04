8b26ad2c424f230c27f424d38c2596f3f870123c
ds3128 <86129628+ds3128@users.noreply.github.com>
2022-05-05 00:48:24 +0100

    ok

--- BusinessLayer/DAOLayer/InterfacesDAO/IInsertDAO.java
+++ BusinessLayer/DAOLayer/InterfacesDAO/IInsertDAO.java
@@ -4,15 +4,31 @@ import BusinessLayer.InterfacesBusiness.*;
 
 public interface IInsertDAO {
 
-	/**
-	 *  
-	 */
-	public abstract void insertUser(String logon, String pass);
+	
+	public abstract void insertUser(IUtilisateur usr);
 
-	public abstract void insertEtudiant(IEtudiant etudiant);
+	public abstract void insertSalle(ISalle salle);
+
+	public abstract void insertFliere(IFiliere filiere);
+
+	public abstract void insertGroupe(IGroupe grp);
+
+	public abstract void insertNiveau(INiveau nivveau);
+
+	public abstract void insertSemestre(ISemestre semestre);
+
+	public abstract void insertAnnee(IAnnee annee);
+
+	public abstract void insertMatiere(IMatiere matiere);
+
+	public abstract void insertDepartement(IDepartement dpt);
 
 	public abstract void insertEnseignant(IEnseignant ens);
 
-	public abstract void insertSuivi(ISuiviCours suivi);
+	public abstract void insertAdministrateur(IAdministrateur admin);
+
+	public abstract void insertEtudiant(IEtudiant etudiant);
+
+	public abstract void insertSuivi(ISuiviCours suiviCour);
 
 }
