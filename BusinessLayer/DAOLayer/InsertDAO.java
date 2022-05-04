bb0025d3e5f4e57af8e422b21f2ec8141e45bf06
ds3128 <86129628+ds3128@users.noreply.github.com>
2022-05-04 23:13:31 +0100

    Insertion dans la base de donnees termine

--- BusinessLayer/DAOLayer/InsertDAO.java
+++ BusinessLayer/DAOLayer/InsertDAO.java
@@ -7,35 +7,171 @@ public class InsertDAO implements IInsertDAO {
 
 
 	/**
-	 * @see BusinessLayer.DAOLayer.IInsertDAO#insertLoginPass(string, String)
+	 * @see BusinessLayer.DAOLayer.IInsertDAO#insertLoginPass(String)
 	 *  
 	 */
-	public void insertUser(String logon, String pass) {
+	public void insertUser(IUtilisateur user) {
+
+		Requete insertRequest = new Requete();
+
+		insertRequest.executeQuery("INSERT INTO Utilisateur VALUES ('" +
+				user.getMatricule() + "', '" +
+				user.getMdp() + "','" +
+				user.getNom() + "', '" +
+				user.getPrenom() + "', '" +
+				user.getEmail() + "''" +
+				user.getDateNais() + "')");
 
 	}
-
-
-	/**
-	 * @see BusinessLayer.DAOLayer.IInsertDAO#insertEtudiant(IEtudiant)
-	 */
-	public void insertEtudiant(IEtudiant etudiant) {
-
-	}
-
-
-	/**
-	 * @see BusinessLayer.DAOLayer.IInsertDAO#insertEnseignant(BusinessLayer.IEnseignant)
-	 */
-	public void insertEnseignant(IEnseignant ens) {
-
-	}
-
 
 	/**
 	 * @see BusinessLayer.DAOLayer.IInsertDAO#insertSuivi(ISuiviCours)
 	 */
 	public void insertSuivi(ISuiviCours suivi) {
 
+		Requete insRequest = new Requete();
+		insRequest.executeQuery("INSERT INTO fairecours VALUES('" +
+				suivi.getSalle().getCodeSalle() + "', '" +
+				suivi.getEnseignant().getMatricule() + "', '" +
+				suivi.getMatiere().getCodeMat() + "', '" +
+				suivi.getGroupe().getIdGrp() + "', '" +
+				suivi.getSemestre().getIdSem() + "', '" +
+				suivi.getJour() + "', '" +
+				suivi.getHeureDebut() + "', '" +
+				suivi.getHeureFin() + "')");
+
 	}
+
+	/**
+	 * @see BusinessLayer.DAOLayer.IInsertDAO#insertEtudiant(IEtudiant)
+	 */
+	public void insertEtudiant(IEtudiant etudiant) {
+
+		Requete insRequest = new Requete();
+		insRequest.executeQuery("INSERT INTO etudiant VALUES('" +
+				etudiant.getMatricule() + "', '" +
+				etudiant.getGroupe().getIdGrp() + "')");
+
+	}
+
+	/**
+	 * @see BusinessLayer.DAOLayer.IInsertDAO#insertEnseignant(BusinessLayer.IEnseignant)
+	 */
+	public void insertEnseignant(IEnseignant ens) {
+
+		Requete insRequest = new Requete();
+		insRequest.executeQuery("INSERT INTO enseignant VALUE('" +
+				ens.getMatricule() + "', '" +
+				ens.getGradeEns() + "', '" +
+				ens.getDepartement().getCodeDepart()+"')");
+
+	}
+
+	@Override
+	public void insertSalle(ISalle salle) {
+		
+		// TODO Auto-generated method stub
+		Requete insRequest = new Requete();
+		insRequest.executeQuery("INSERT INTO salle VALUES('" +
+				salle.getCodeSalle() + "', '" +
+				salle.getNomSalle() + "', '" +
+				salle.getType() + "', " +
+				salle.getCapacite() + ")");
+
+	}
+
+	@Override
+	public void insertFliere(IFiliere filiere) {
+
+		// TODO Auto-generated method stub
+		Requete insRequest = new Requete();
+		insRequest.executeQuery("INSERT INTO filiere VALUES('" +
+				filiere.getCodeFil() + "', '" +
+				filiere.getNomFil() + "')");
+		
+	}
+
+	@Override
+	public void insertGroupe(IGroupe grp) {
+
+		// TODO Auto-generated method stub
+		Requete insRequest = new Requete();
+		insRequest.executeQuery("INSERT INTO groupe VALUES(" +
+				grp.getCodeGrp() + ", '" +
+				grp.getNomGrp() + "', " +
+				grp.getEffectifGrp() + ", " +
+				grp.getNiveau().getIdNiv()+")");
+		
+	}
+
+	@Override
+	public void insertNiveau(INiveau nivveau) {
+
+		// TODO Auto-generated method stub
+		Requete insRequest = new Requete();
+		insRequest.executeQuery("INSERT INTO niveau VALUES(" +
+				nivveau.getIdNiv() + ", '" +
+				nivveau.getCodeNiv() + "', '" +
+				nivveau.getNomNiv() + "', '" +
+				nivveau.getFiliere().getCodeFil()+"')");
+		
+	}
+
+	@Override
+	public void insertSemestre(ISemestre semestre) {
+
+		// TODO Auto-generated method stub
+		Requete insRequest = new Requete();
+		insRequest.executeQuery("INSERT INTO semestre VALUES(" +
+				semestre.getIdSem() + ",'"
+				+ semestre.getCodeSem() + "', '" +
+				semestre.getNomSem() + "'," +
+				semestre.getAnnee().getIdAnnee()+")");
+		
+	}
+
+	@Override
+	public void insertAnnee(IAnnee annee) {
+
+		// TODO Auto-generated method stub
+		Requete insRequest = new Requete();
+		insRequest.executeQuery("INSERT INTO annee VALUES(" +
+				annee.getIdAnnee() + ", '" +
+				annee.getAnneeDebut() + "', '" +
+				annee.getAnneeFin()+"')");
+		
+	}
+
+	@Override
+	public void insertMatiere(IMatiere matiere) {
+
+		// TODO Auto-generated method stub
+		Requete insRequest = new Requete();
+		insRequest.executeQuery("INSERT INTO matiere VALUES('" +
+				matiere.getCodeMat() + "', '" +
+				matiere.getIntituleMat()+"')");
+		
+	}
+
+	@Override
+	public void insertDepartement(IDepartement dpt) {
+
+		// TODO Auto-generated method stub
+		Requete insRequest = new Requete();
+		insRequest.executeQuery("INSERT INTO departement VALUES('" +
+				dpt.getCodeDepart() + "', '" +
+				dpt.getNomDepart()+"')");
+		
+	}
+
+	@Override
+	public void insertAdministrateur(IAdministrateur admin) {
+
+		// TODO Auto-generated method stub
+		Requete insRequest = new Requete();
+		insRequest.executeQuery("INSERT INTO administrateur VALUES('"+admin.getMatricule()+"')");
+		
+	}
+
 
 }
