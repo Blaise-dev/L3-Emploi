8ea8991f3ae43eddc9ef44cd2a9b546a1bc4d55d
ds3128 <86129628+ds3128@users.noreply.github.com>
2022-05-05 00:48:11 +0100

    Requete de suppression dans la BD termine

--- BusinessLayer/DAOLayer/DeleteDAO.java
+++ BusinessLayer/DAOLayer/DeleteDAO.java
@@ -11,6 +11,9 @@ public class DeleteDAO implements IDeleteDAO {
 	 */
 	public void delSuivi(ISuiviCours suivi) {
 
+		Requete delRequest = new Requete();
+		delRequest.executeQuery("DELETE * FROM fairecour WHERE codeSalle = '"+suivi.getSalle().getCodeSalle()+"'");
+
 	}
 
 
@@ -18,6 +21,9 @@ public class DeleteDAO implements IDeleteDAO {
 	 * @see BusinessLayer.DAOLayer.IDeleteDAO#delEnseignant(BusinessLayer.IEnseignant)
 	 */
 	public void delEnseignant(IEnseignant ens) {
+
+		Requete delRequest = new Requete();
+		delRequest.executeQuery("DELETE * FROM enseignant WHERE matricule = '"+ens.getMatricule()+"'");
 
 	}
 
@@ -27,6 +33,109 @@ public class DeleteDAO implements IDeleteDAO {
 	 */
 	public void delEtudiant(IEtudiant etudiant) {
 
+		Requete delRequest = new Requete();
+		delRequest.executeQuery("DELETE * FROM etudian WHERE matricule = '"+etudiant.getMatricule()+"'");
+
+	}
+
+
+	@Override
+	public void deltUser(IUtilisateur usr) {
+
+		// TODO Auto-generated method stub
+		Requete delRequest = new Requete();
+		delRequest.executeQuery("DELETE * FROM utilisateur WHERE matricule = '" + usr.getMatricule() + "'");
+
+	}
+
+
+	@Override
+	public void delSalle(ISalle salle) {
+
+		// TODO Auto-generated method stub
+		Requete delRequest = new Requete();
+		delRequest.executeQuery("DELETE * FROM salle WHERE codeSalle = '"+salle.getCodeSalle()+"' ");
+		
+	}
+
+
+	@Override
+	public void delFliere(IFiliere filiere) {
+
+		// TODO Auto-generated method stub
+		Requete delRequest = new Requete();
+		delRequest.executeQuery("DELETE * FROM filiere WHERE codeFiliere = '"+filiere.getCodeFil()+"'");
+		
+	}
+
+
+	@Override
+	public void delGroupe(IGroupe grp) {
+
+		// TODO Auto-generated method stub
+		Requete delRequest = new Requete();
+		delRequest.executeQuery("DELETE * FROM groupe WHERE idGrp = '"+grp.getCodeGrp()+"'");
+		
+	}
+
+
+	@Override
+	public void delNiveau(INiveau nivveau) {
+
+		// TODO Auto-generated method stub
+		Requete delRequest = new Requete();
+		delRequest.executeQuery("DELETE * FROM niveau WHERE idNiveau = " + nivveau.getIdNiv());
+		
+	}
+
+
+	@Override
+	public void delSemestre(ISemestre semestre) {
+
+		// TODO Auto-generated method stub
+		Requete delRequest = new Requete();
+		delRequest.executeQuery("DELETE * semestre WHERE idSem = " + semestre.getIdSem());
+		
+	}
+
+
+	@Override
+	public void delAnnee(IAnnee annee) {
+
+		// TODO Auto-generated method stub
+		Requete delRequest = new Requete();
+		delRequest.executeQuery("DELETE * FROM annee WHERE idAnnee = " +annee.getIdAnnee());
+		
+	}
+
+
+	@Override
+	public void delMatiere(IMatiere matiere) {
+
+		// TODO Auto-generated method stub
+		Requete delRequest = new Requete();
+		delRequest.executeQuery("DELETE * FROM matiere WHERE codeMatiere = '"+ matiere.getCodeMat() +"'");
+		
+	}
+
+
+	@Override
+	public void delDepartement(IDepartement dpt) {
+
+		// TODO Auto-generated method stub
+		Requete delRequest = new Requete();
+		delRequest.executeQuery("DELETE * FROM departement WHERE codeDepartement = '"+dpt.getCodeDepart()+"'");
+		
+	}
+
+
+	@Override
+	public void delAdministrateur(IAdministrateur admin) {
+
+		// TODO Auto-generated method stub
+		Requete delRequest = new Requete();
+		delRequest.executeQuery("DELETE * FROM administrateur WHERE matricule = '"+admin.getMatricule()+"'");
+		
 	}
 
 }
