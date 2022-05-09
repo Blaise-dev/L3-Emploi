cf5a1257dfa8d20b3a07664fce9e47bac990d808
ds3128 <86129628+ds3128@users.noreply.github.com>
2022-05-09 18:37:08 +0100

    correction des fichiers supprimes

--- src/BusinessLayer/DAOLayer/DeleteDAO.java
+++ src/BusinessLayer/DAOLayer/DeleteDAO.java
@@ -11,6 +11,17 @@ public class DeleteDAO implements IDeleteDAO {
 	 */
 	public void delSuivi(ISuiviCours suivi) {
 
+		Requete delRequest = new Requete();
+		delRequest.executeUpdate("DELETE FROM fairecours WHERE codeSalle = '" +
+				suivi.getSalle().getCodeSalle() + "' AND matricule = '" +
+				suivi.getEnseignant().getMatricule() + "' AND codeMat = '" +
+				suivi.getMatiere().getCodeMat() + "' AND idGrp = " +
+				suivi.getGroupe().getIdGrp() + " AND idSem = " +
+				suivi.getSemestre().getIdSem() + " AND jour = '" +
+				suivi.getJour() + "' AND heureDebut = '" +
+				suivi.getHeureDebut() + "' AND heureFin = '" +
+				suivi.getHeureFin()+"'");
+
 	}
 
 
@@ -18,6 +29,9 @@ public class DeleteDAO implements IDeleteDAO {
 	 * @see projet.BusinessLayer.DAOLayer.IDeleteDAO#delEnseignant(projet.BusinessLayer.IEnseignant)
 	 */
 	public void delEnseignant(IEnseignant ens) {
+
+		Requete delRequest = new Requete();
+		delRequest.executeUpdate("DELETE FROM enseignant WHERE matricule = '"+ens.getMatricule()+"'");
 
 	}
 
@@ -27,6 +41,109 @@ public class DeleteDAO implements IDeleteDAO {
 	 */
 	public void delEtudiant(IEtudiant etudiant) {
 
+		Requete delRequest = new Requete();
+		delRequest.executeUpdate("DELETE FROM etudiant WHERE matricule = '"+etudiant.getMatricule()+"'");
+
+	}
+
+
+	@Override
+	public void deltUser(IUtilisateur usr) {
+
+		// TODO Auto-generated method stub
+		Requete delRequest = new Requete();
+		delRequest.executeUpdate("DELETE FROM utilisateur WHERE matricule = '" + usr.getMatricule() + "'");
+
+	}
+
+
+	@Override
+	public void delSalle(ISalle salle) {
+
+		// TODO Auto-generated method stub
+		Requete delRequest = new Requete();
+		delRequest.executeUpdate("DELETE FROM salle WHERE codeSalle = '"+salle.getCodeSalle()+"' ");
+		
+	}
+
+
+	@Override
+	public void delFliere(IFiliere filiere) {
+
+		// TODO Auto-generated method stub
+		Requete delRequest = new Requete();
+		delRequest.executeUpdate("DELETE FROM filiere WHERE codeFiliere = '"+filiere.getCodeFil()+"'");
+		
+	}
+
+
+	@Override
+	public void delGroupe(IGroupe grp) {
+
+		// TODO Auto-generated method stub
+		Requete delRequest = new Requete();
+		delRequest.executeUpdate("DELETE FROM groupe WHERE idGrp = '"+grp.getCodeGrp()+"'");
+		
+	}
+
+
+	@Override
+	public void delNiveau(INiveau niveau) {
+
+		// TODO Auto-generated method stub
+		Requete delRequest = new Requete();
+		delRequest.executeUpdate("DELETE FROM niveau WHERE idNiveau = " + niveau.getIdNiv());
+		
+	}
+
+
+	@Override
+	public void delSemestre(ISemestre semestre) {
+
+		// TODO Auto-generated method stub
+		Requete delRequest = new Requete();
+		delRequest.executeUpdate("DELETE FROM semestre WHERE idSem = " + semestre.getIdSem());
+		
+	}
+
+
+	@Override
+	public void delAnnee(IAnnee annee) {
+
+		// TODO Auto-generated method stub
+		Requete delRequest = new Requete();
+		delRequest.executeUpdate("DELETE FROM annee WHERE idAnnee = " +annee.getIdAnnee());
+		
+	}
+
+
+	@Override
+	public void delMatiere(IMatiere matiere) {
+
+		// TODO Auto-generated method stub
+		Requete delRequest = new Requete();
+		delRequest.executeUpdate("DELETE FROM matiere WHERE codeMatiere = '"+ matiere.getCodeMat() +"'");
+		
+	}
+
+
+	@Override
+	public void delDepartement(IDepartement dpt) {
+
+		// TODO Auto-generated method stub
+		Requete delRequest = new Requete();
+		delRequest.executeUpdate("DELETE FROM departement WHERE codeDepartement = '" + dpt.getCodeDepart() + "'");
+		
+	}
+
+
+	@Override
+	public void delAdministrateur(IAdministrateur admin) {
+
+		// TODO Auto-generated method stub
+		Requete delRequest = new Requete();
+		delRequest.executeUpdate("DELETE FROM administrateur WHERE matricule = '" + admin.getMatricule() + "'");
+		
 	}
 
 }
--- src/BusinessLayer/DAOLayer/InsertDAO.java
+++ src/BusinessLayer/DAOLayer/InsertDAO.java
@@ -5,37 +5,203 @@ import BusinessLayer.InterfacesBusiness.*;
 
 public class InsertDAO implements IInsertDAO {
 
+	public void insertUser(IUtilisateur user) {
 
-	/**
-	 * @see projet.BusinessLayer.DAOLayer.IInsertDAO#insertLoginPass(string, String)
-	 *  
-	 */
-	public void insertUser(String logon, String pass) {
+		ISelectDAO selection = new SelectDAO();
+		if(!selection.exist("Utilisateur", "matricule", user.getMatricule())) {
+			Requete insertRequest = new Requete();
+	
+			insertRequest.executeUpdate("INSERT INTO Utilisateur VALUES ('" +
+					user.getMatricule() + "', '" +
+					user.getMdp() + "','" +
+					user.getNom() + "', '" +
+					user.getPrenom() + "', '" +
+					user.getEmail() + "', '" +
+					user.getDateNais() + "')");
+		}
 
 	}
 
+	/**
+	 * @see BusinessLayer.DAOLayer.IInsertDAO#insertSuivi(ISuiviCours)
+	 */
+	public void insertSuivi(ISuiviCours suivi) {
+
+		insertSalle(suivi.getSalle());
+		insertEnseignant(suivi.getEnseignant());
+		insertMatiere(suivi.getMatiere());
+		insertGroupe(suivi.getGroupe());
+		insertSemestre(suivi.getSemestre());
+		Requete insRequest = new Requete();
+		insRequest.executeUpdate("INSERT INTO fairecours VALUES('" +
+				suivi.getSalle().getCodeSalle() + "', '" +
+				suivi.getEnseignant().getMatricule() + "', '" +
+				suivi.getMatiere().getCodeMat() + "', " +
+				suivi.getGroupe().getIdGrp() + ", " +
+				suivi.getSemestre().getIdSem() + ", '" +
+				suivi.getJour() + "', '" +
+				suivi.getHeureDebut() + "', '" +
+				suivi.getHeureFin() + "')");
+
+	}
 
 	/**
 	 * @see projet.BusinessLayer.DAOLayer.IInsertDAO#insertEtudiant(IEtudiant)
 	 */
 	public void insertEtudiant(IEtudiant etudiant) {
+		ISelectDAO selection = new SelectDAO();
+		if(!selection.exist("Utilisateur", "matricule", etudiant.getMatricule()))
+			insertUser(etudiant);
+		if(!selection.exist("Etudiant", "matricule", etudiant.getMatricule())) {
+			Requete insRequest = new Requete();
+			insRequest.executeUpdate("INSERT INTO etudiant VALUES('" +
+					etudiant.getMatricule() + "', '" +
+					etudiant.getGroupe().getIdGrp() + "')");
+		}
 
 	}
-
 
 	/**
 	 * @see projet.BusinessLayer.DAOLayer.IInsertDAO#insertEnseignant(projet.BusinessLayer.IEnseignant)
 	 */
 	public void insertEnseignant(IEnseignant ens) {
+		ISelectDAO selection = new SelectDAO();
+		if(!selection.exist("Utilisateur", "matricule", ens.getMatricule()))
+			insertUser(ens);
+		if(!selection.exist("Enseignant", "matricule", ens.getMatricule())) {
+			Requete insRequest = new Requete();
+			insRequest.executeUpdate("INSERT INTO enseignant VALUES('" +
+					ens.getMatricule() + "', '" +
+					ens.getGradeEns() + "', '" +
+					ens.getDepartement().getCodeDepart()+"')");
+		}
 
 	}
 
-
-	/**
-	 * @see projet.BusinessLayer.DAOLayer.IInsertDAO#insertSuivi(ISuiviCours)
-	 */
-	public void insertSuivi(ISuiviCours suivi) {
-
+	@Override
+	public void insertSalle(ISalle salle) {
+		
+		// TODO Auto-generated method stub
+		ISelectDAO selection = new SelectDAO();
+		if(!selection.exist("Salle", "codeSalle", salle.getCodeSalle())) {
+			Requete insRequest = new Requete();
+			insRequest.executeUpdate("INSERT INTO salle VALUES('" +
+					salle.getCodeSalle() + "', '" +
+					salle.getNomSalle() + "', '" +
+					salle.getType() + "', " +
+					salle.getCapacite() + ")");
+		}
 	}
+
+	@Override
+	public void insertFiliere(IFiliere filiere) {
+
+		// TODO Auto-generated method stub
+		ISelectDAO selection = new SelectDAO();
+		if(!selection.exist("Filiere", "codeFiliere", filiere.getCodeFil())) {
+			Requete insRequest = new Requete();
+			insRequest.executeUpdate("INSERT INTO filiere VALUES('" +
+					filiere.getCodeFil() + "', '" +
+					filiere.getNomFil() + "')");
+		}
+	}
+	
+
+	@Override
+	public void insertGroupe(IGroupe grp) {
+
+		// TODO Auto-generated method stub
+		insertNiveau(grp.getNiveau());
+		Requete insRequest = new Requete();
+		insRequest.executeUpdate("INSERT INTO groupe VALUES('" +
+				grp.getCodeGrp() + "', '" +
+				grp.getNomGrp() + "', " +
+				grp.getEffectifGrp() + ", " +
+				grp.getNiveau().getIdNiv()+")");
+		
+	}
+
+	@Override
+	public void insertNiveau(INiveau niveau) {
+
+		// TODO Auto-generated method stub
+		ISelectDAO selection = new SelectDAO();
+		if(!selection.exist("Filiere", "codeFiliere", niveau.getCodeFiliere()))
+			insertFiliere(niveau.getFiliere());
+		Requete insRequest = new Requete();
+		insRequest.executeUpdate("INSERT INTO niveau VALUES('" +
+				niveau.getCodeNiv() + "', '" +
+				niveau.getNomNiv() + "', '" +
+				niveau.getFiliere().getCodeFil()+"')");
+		
+	}
+
+	@Override
+	public void insertSemestre(ISemestre semestre) {
+
+		// TODO Auto-generated method stub
+		insertAnnee(semestre.getAnnee());
+		Requete insRequest = new Requete();
+		insRequest.executeUpdate("INSERT INTO semestre VALUES('" +
+				semestre.getCodeSem() + "', '" +
+				semestre.getNomSem() + "', " +
+				semestre.getAnnee().getIdAnnee()+")");
+		
+	}
+
+	@Override
+	public void insertAnnee(IAnnee annee) {
+
+		// TODO Auto-generated method stub
+		Requete insRequest = new Requete();
+		insRequest.executeUpdate("INSERT INTO annee VALUES('" +
+				annee.getAnneeDebut() + "', '" +
+				annee.getAnneeFin()+"')");
+		
+	}
+
+	@Override
+	public void insertMatiere(IMatiere matiere) {
+
+		// TODO Auto-generated method stub
+		ISelectDAO selection = new SelectDAO();
+		if(!selection.exist("Departement", "codeDepartement", matiere.getDepartement().getCodeDepart()))
+			insertDepartement(matiere.getDepartement());
+		Requete insRequest = new Requete();
+		if(!selection.exist("Matiere", "codeMatiere", matiere.getCodeMat())) {
+			insRequest.executeUpdate("INSERT INTO matiere VALUES('" +
+					matiere.getCodeMat() + "', '" +
+					matiere.getIntituleMat() + "', '" +
+					matiere.getDepartement().getCodeDepart()+"')");
+		}
+		
+	}
+
+	@Override
+	public void insertDepartement(IDepartement dpt) {
+
+		// TODO Auto-generated method stub
+		Requete insRequest = new Requete();
+		insRequest.executeUpdate("INSERT INTO departement VALUES('" +
+				dpt.getCodeDepart() + "', '" +
+				dpt.getNomDepart()+"')");
+		
+	}
+
+	@Override
+	public void insertAdministrateur(IAdministrateur admin) {
+
+		// TODO Auto-generated method stub
+		ISelectDAO selection = new SelectDAO();
+		if(!selection.exist("Utilisateur", "matricule", admin.getMatricule()))
+			insertUser(admin);
+		if(!selection.exist("Administrateur", "matricule", admin.getMatricule())) {
+			Requete insRequest = new Requete();
+			insRequest.executeUpdate("INSERT INTO administrateur VALUES('" +
+					admin.getMatricule()+"')");
+		}
+		
+	}
+
 
 }
